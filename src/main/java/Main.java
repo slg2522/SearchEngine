13a2c90a736bd6a381587392705fd7aa22211f6e
nguyen123-it <novavietnam123@gmail.com>
2021-02-05 18:37:00 -0500

    take query input from user

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,15 @@
 import java.util.Set;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter search query: ");
+        String query = scanner.nextLine();
         SearchDemo obj = new SearchDemo();
-        Set<String> result = obj.getDataFromGoogle("soccer");
+        Set<String> result = obj.getDataFromGoogle(query);
         for(String temp : result){
             System.out.println(temp);
         }

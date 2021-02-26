33761b4f27f4592255a2d2316e9c7bf7fb0ae4c7
nguyen123-it <novavietnam123@gmail.com>
2021-02-26 13:49:30 -0500

    add methods for creating database and table

--- src/main/java/DataBase.java
+++ src/main/java/DataBase.java
@@ -1,11 +1,28 @@
+import javax.xml.crypto.Data;
 import java.sql.*;
 
 public class DataBase {
-    public static void connect() {
+
+    private static final String databaseUrl = "jdbc:sqlite:";
+    private static final String databaseName = "sampleDataBase.db";
+    private static DataBase sampleDataBase;
+
+    private DataBase(){
+        createTable();
+    }
+
+    public static DataBase getSampleDataBaseInstance(){
+        if (sampleDataBase == null){
+            sampleDataBase = new DataBase();
+        }
+        return sampleDataBase;
+    }
+
+    /*public static void connect() {
         Connection conn = null;
         try {
             // db parameters
-            String url = "jdbc:sqlite:C:\\Users\\hoang\\.m2\\repository\\org\\xerial\\sqlite-jdbc\\sampleDataBase.db";
+            String url = "jdbc:sqlite:sampleDataBase.db";
             // create a connection to the database
             conn = DriverManager.getConnection(url);
 
@@ -23,10 +40,38 @@ public class DataBase {
             }
         }
     }
+
+     */
+
+    private Connection getConnection(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            return DriverManager.getConnection(databaseUrl+databaseName);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void createTable() {
+        String createStatement =
+                "CREATE TABLE IF NOT EXISTS `shopping_list_entry` ( " +
+                        "`checked` INTEGER NOT NULL, " +
+                        "`amount` INTEGER NOT NULL, " +
+                        "`title` TEXT NOT NULL);";
+        try (
+                Connection connection = getConnection();
+                Statement statement = connection.createStatement()) {
+            statement.executeUpdate(createStatement);
+
+        } catch (Exception e) {
+            System.exit(0);
+        }
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        connect();
+        //connect();
     }
 }

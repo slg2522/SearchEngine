612df21cc1a4c632f33cba9e2d6fd798cae16811
nguyen123-it <novavietnam123@gmail.com>
2021-02-26 14:41:48 -0500

    delete unecessary method named connect()

--- src/main/java/DataBase.java
+++ src/main/java/DataBase.java
@@ -18,31 +18,6 @@ public class DataBase {
         return sampleDataBase;
     }
 
-    /*public static void connect() {
-        Connection conn = null;
-        try {
-            // db parameters
-            String url = "jdbc:sqlite:sampleDataBase.db";
-            // create a connection to the database
-            conn = DriverManager.getConnection(url);
-
-            System.out.println("Connection to SQLite has been established.");
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
-        }
-    }
-
-     */
-
     // Connection is used to perform database-related operations(crete table, delete data, insert data,...)
     private Connection getConnection(){
         try {

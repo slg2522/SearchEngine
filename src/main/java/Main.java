0fbd88e6e5b69cc7c121c996349c895a434a5545
nguyen123-it <novavietnam123@gmail.com>
2021-02-26 14:39:59 -0500

    successfully test method displayData()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,14 +5,18 @@ public class Main {
 
     public static void main(String[] args) {
 
+        DataBase dataBase = DataBase.getSampleDataBaseInstance();
         Scanner keyboard = new Scanner(System.in);
         System.out.print("Enter search query: ");
         String query = keyboard.nextLine();
         SearchDemo obj = new SearchDemo();
         Set<String> result = obj.getDataFromGoogle(query);
         for(String temp : result){
-            System.out.println(temp);
+            //System.out.println(temp);
+            dataBase.insert(temp);
         }
         System.out.println(result.size());
+        // test method displayData
+        dataBase.displayData();
     }
 }
